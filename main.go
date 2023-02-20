36dab92cdab0f8c009cc216c42874f3eef3b0617
Noah Pratt <prattnj@gmail.com>
2023-02-19 19:24:55 -0700

    added daos

--- main.go
+++ main.go
@@ -2,19 +2,11 @@ package main
 
 import (
 	"github.com/labstack/echo/v4"
-	"github.com/labstack/echo/v4/middleware"
 	"github.com/prattnj/fms-go/handler"
-	"golang.org/x/crypto/acme/autocert"
-	"log"
-	"net/http"
 )
 
 func main() {
 	e := echo.New()
-
-	e.AutoTLSManager.Cache = autocert.DirCache("/var/www/.cache")
-	e.Use(middleware.Recover())
-	e.Use(middleware.Logger())
 
 	e.HTTPErrorHandler = handler.HandleNotFound
 	e.Static("/", "./web")
@@ -27,15 +19,6 @@ func main() {
 	e.GET("/person/:personID", handler.HandlePersonID)
 	e.POST("/user/login", handler.HandleLogin)
 	e.POST("/user/register", handler.HandleRegister)
-	e.GET("/", func(c echo.Context) error {
-		return c.HTML(http.StatusOK, `
-			<h1>Welcome to Echo!</h1>
-			<h3>TLS certificates automatically installed from Let's Encrypt :)</h3>
-		`)
-	})
 
-	err := e.StartAutoTLS(":3003")
-	if err != nil {
-		log.Fatal(err)
-	}
+	e.Logger.Fatal(e.Start(":3003"))
 }
