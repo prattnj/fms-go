95d92944322aa2c19686e82ef89e9e09e4849e06
Noah Pratt <prattnj@gmail.com>
2023-02-20 14:42:22 -0700

    finished authtoken dao

--- dal/dal_test.go
+++ dal/dal_test.go
@@ -8,27 +8,37 @@ import (
 func TestAuthtoken(t *testing.T) {
 	db := Db()
 	tx, err := db.Begin()
-	if err != nil {
-		t.Error(err)
-	}
+	handleTestError(t, err)
 	token := model.AuthToken{AuthToken: "test", Username: "njpratt"}
+	err = T_clear(tx)
+	handleTestError(t, err)
 	err = T_insert(tx, token)
-	if err != nil {
-		t.Error(err)
-	}
+	handleTestError(t, err)
 	token2, err := T_find(tx, "test")
-	if err != nil {
-		t.Error(err)
-	}
+	handleTestError(t, err)
 	if token2.AuthToken != token.AuthToken {
 		t.Error("Token not found")
 	}
 	err = T_clear(tx)
-	if err != nil {
-		t.Error(err)
-	}
+	handleTestError(t, err)
 	err = tx.Rollback()
 	err = DbClose(db)
+	handleTestError(t, err)
+}
+
+func TestEvent(t *testing.T) {
+
+}
+
+func TestPerson(t *testing.T) {
+
+}
+
+func TestUser(t *testing.T) {
+
+}
+
+func handleTestError(t *testing.T, err error) {
 	if err != nil {
 		t.Error(err)
 	}
