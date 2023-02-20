14266000879dbcd08c0858b3f45ed7b1fa2b4a61
Noah Pratt <prattnj@gmail.com>
2023-02-20 12:50:13 -0700

    finished authtoken dao

--- dal/authtoken-da.go
+++ dal/authtoken-da.go
@@ -5,26 +5,48 @@ import (
 	"github.com/prattnj/fms-go/model"
 )
 
-func T_insert(db *sql.DB, token model.AuthToken) {
+func T_insert(db *sql.DB, token model.AuthToken) error {
 	// todo add token
+	stmt, err := db.Prepare("INSERT INTO Authtoken (authtoken, username) VALUES(?,?);")
+	if err != nil {
+		return err
+	}
+	_, err = stmt.Exec(token.AuthToken, token.Username)
+	if err != nil {
+		return err
+	}
+	err = stmt.Close()
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
-func T_find(db *sql.DB, token string) model.AuthToken {
+func T_find(db *sql.DB, token string) (model.AuthToken, error) {
 	// todo find token
-	return model.AuthToken{}
-}
-
-func T_getUsername(db *sql.DB, token string) string {
-	// todo get username
-	return ""
+	stmt, err := db.Prepare("SELECT * FROM Authtoken WHERE authtoken = ?;")
+	if err != nil {
+		return model.AuthToken{}, err
+	}
+	var authToken string
+	var username string
+	err = stmt.QueryRow(token).Scan(&authToken, &username)
+	if err != nil {
+		return model.AuthToken{}, err
+	}
+	return model.AuthToken{AuthToken: authToken, Username: username}, nil
 }
 
 func T_clear(db *sql.DB) error {
-	stmt, err := db.Prepare("DELETE FROM authtoken")
+	stmt, err := db.Prepare("DELETE FROM authtoken;")
 	if err != nil {
 		return err
 	}
 	_, err = stmt.Exec()
+	if err != nil {
+		return err
+	}
+	err = stmt.Close()
 	if err != nil {
 		return err
 	}
---
+++ dal/dal_test.go
@@ -0,0 +1,31 @@
+package dal
+
+import (
+	"github.com/prattnj/fms-go/model"
+	"testing"
+)
+
+func TestAuthtoken(t *testing.T) {
+	// todo test authtoken
+	db := Db()
+	token := model.AuthToken{AuthToken: "test", Username: "njpratt"}
+	err := T_insert(db, token)
+	if err != nil {
+		t.Error(err)
+	}
+	token2, err := T_find(db, "test")
+	if err != nil {
+		t.Error(err)
+	}
+	if token2.AuthToken != token.AuthToken {
+		t.Error("Token not found")
+	}
+	err = T_clear(db)
+	if err != nil {
+		t.Error(err)
+	}
+	err = DbClose(db)
+	if err != nil {
+		t.Error(err)
+	}
+}
