0dc4b66ed20ef34ac907be12794ee295d7bb8d15
Noah Pratt <prattnj@gmail.com>
2023-02-20 14:57:58 -0700

    finished authtoken dao

--- dal/dal_test.go
+++ dal/dal_test.go
@@ -11,31 +11,31 @@ func TestAuthtoken(t *testing.T) {
 	// Start transaction / set up
 	db := Db()
 	tx, err := db.Begin()
-	handleTestError(t, err)
+	handleTestError(t, err, 14)
 	token := model.AuthToken{AuthToken: "test", Username: "njpratt"}
 	err = T_clear(tx)
-	handleTestError(t, err)
+	handleTestError(t, err, 17)
 
 	// Test insert and find
 	err = T_insert(tx, token)
-	handleTestError(t, err)
+	handleTestError(t, err, 21)
 	token2, err := T_find(tx, "test")
-	handleTestError(t, err)
+	handleTestError(t, err, 23)
 	if token2.AuthToken != token.AuthToken {
 		t.Error("Token not found")
 	}
 
 	// Test clear and find
 	err = T_clear(tx)
-	handleTestError(t, err)
+	handleTestError(t, err, 30)
 	token3, err := T_find(tx, "test")
 	if token3.AuthToken != "" {
 		t.Error("Token not cleared")
 	}
 	err = tx.Rollback()
-	handleTestError(t, err)
+	handleTestError(t, err, 36)
 	err = DbClose(db)
-	handleTestError(t, err)
+	handleTestError(t, err, 38)
 }
 
 func TestEvent(t *testing.T) {
@@ -50,10 +50,10 @@ func TestUser(t *testing.T) {
 
 }
 
-func handleTestError(t *testing.T, err error) {
+func handleTestError(t *testing.T, err error, line int) {
 	if err != nil {
 		t.Error(err)
 		fmt.Println(err)
-		fmt.Print("RETURNING...\n")
+		fmt.Printf("RETURNING (%d)...\n", line)
 	}
 }
