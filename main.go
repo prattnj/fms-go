a68e389e4501b64725d8c289e74951e5d91a7c55
Noah Pratt <prattnj@gmail.com>
2023-02-19 19:19:30 -0700

    added daos

--- main.go
+++ main.go
@@ -2,13 +2,19 @@ package main
 
 import (
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 	"github.com/prattnj/fms-go/handler"
+	"golang.org/x/crypto/acme/autocert"
 	"log"
 	"net/http"
 )
 
 func main() {
 	e := echo.New()
+
+	e.AutoTLSManager.Cache = autocert.DirCache("/var/www/.cache")
+	e.Use(middleware.Recover())
+	e.Use(middleware.Logger())
 
 	e.HTTPErrorHandler = handler.HandleNotFound
 	e.Static("/", "./web")
