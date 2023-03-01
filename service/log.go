4f7d394cad9a8edf723ec3ae1afd5ae766bfc8ad
pratt <prattnj@gmail.com>
2023-03-01 09:35:32 -0700

    added logging error

--- service/log.go
+++ service/log.go
@@ -3,6 +3,7 @@ package service
 import (
 	"github.com/prattnj/fms-go/dal"
 	"github.com/prattnj/fms-go/model"
+	"log"
 	"time"
 )
 
@@ -10,16 +11,19 @@ func Log(endpoint string, ipv4 string, success bool) model.GenericResponse {
 
 	db := dal.Db()
 	if db == nil {
+		logError("Could not connect to database")
 		return serverError
 	}
 	tx, err := db.Begin()
 	if err != nil {
+		logError(err.Error())
 		return serverError
 	}
 
-	log := model.Log{Timestamp: time.Now().Unix(), Endpoint: endpoint, IPv4: ipv4, Success: success}
-	err = dal.L_insert(tx, log)
+	logObj := model.Log{Timestamp: time.Now().Unix(), Endpoint: endpoint, IPv4: ipv4, Success: success}
+	err = dal.L_insert(tx, logObj)
 	if err != nil {
+		logError(err.Error())
 		err := tx.Rollback()
 		if err != nil {
 			return serverError
@@ -29,12 +33,20 @@ func Log(endpoint string, ipv4 string, success bool) model.GenericResponse {
 
 	err = tx.Commit()
 	if err != nil {
+		logError(err.Error())
 		return serverError
 	}
 	err = dal.DbClose(db)
 	if err != nil {
+		logError(err.Error())
 		return serverError
 	}
 	return model.GenericResponse{Success: true, Message: "Log succeeded"}
 
+}
+
+func logError(err string) {
+	if err != "" {
+		log.Printf("FMS-GO LOGGING ERROR: %s\n", err)
+	}
 }
